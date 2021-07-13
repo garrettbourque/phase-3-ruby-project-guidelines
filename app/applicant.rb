3de2b5cd336a2c25b52a09b2c7b04dd916acabef
garrettBourque <bourque.garrett@gmail.com>
2021-07-13 11:44:39 -0500

    checking branch

--- app/applicant.rb
+++ app/applicant.rb
@@ -1,5 +1,5 @@
 class Applicant < ActiveRecord::Base
 has_many :adopts
-has_many :childs, thorugh: :adopts
+has_many :childs, through: :adopts
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
-Child.destroy_all
+puts "Deleting previous records..."
+
 Applicant.destroy_all
-Adopt.destroy_all
-Foster.destroy_all
-FosterFamily.destroy_all
+Child.destroy_all
+#Adopt.destroy_all
+#Foster.destroy_all
+#FosterFamily.destroy_all
 
 puts "Seeding Applicants..."
 Applicant.create(name: "Bill", income: 40000, address:"254 South first St.")
@@ -14,17 +16,17 @@ Applicant.create(name: "Pat", income: 60000, address:"666 Bible St.")
 
 puts "Seeding Child..."
 Child.create(name: "Allen", age: 4, sex: "male")
-Child.create(name: "Garrett", age: 2, sex: "male")
-Child.create(name: "Suey", age: 3, sex: "female")
-Child.create(name: "Joyce", age: 9, sex: "female")
-Child.create(name: "Alex", age: 3, sex: "male")
-Child.create(name: "Jaz", age: 8, sex: "female")
-Child.create(name: "Sean B", age: 11, sex: "male")
-Child.create(name: "Sean S", age: 7, sex: "male")
-Child.create(name: "Brianna", age: 10, sex: "female")
-Child.create(name: "Daniel", age: 1, sex: "male")
-Child.create(name: "Hadi", age: 12, sex: "male")
-Child.create(name: "Nahum", age: 6, sex: "male")
+#Child.create(name: "Garrett", age: 2, sex: "male")
+#Child.create(name: "Suey", age: 3, sex: "female")
+#Child.create(name: "Joyce", age: 9, sex: "female")
+#Child.create(name: "Alex", age: 3, sex: "male")
+#Child.create(name: "Jaz", age: 8, sex: "female")
+#Child.create(name: "Sean B", age: 11, sex: "male")
+#Child.create(name: "Sean S", age: 7, sex: "male")
+#Child.create(name: "Brianna", age: 10, sex: "female")
+#Child.create(name: "Daniel", age: 1, sex: "male")
+#Child.create(name: "Hadi", age: 12, sex: "male")
+#Child.create(name: "Nahum", age: 6, sex: "male")
 
 
 puts "Seeding FosterFamily..."
